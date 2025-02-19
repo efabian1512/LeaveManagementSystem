a03cac975fb395f1d35944ca76971f8206089468
efabian1512 <efabian1989@hotmail.com>
2025-02-18 21:49:17 -0400

    first commit

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Studentof MVC Mastery",
+                DateOfBirth = new DateTime(1954, 12, 01)
+            };
+            return View(data);
+        }
+
+    }
+}
+
---
+++ LeaveManagementSystem.Web/Models/TestViewMode.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+}
